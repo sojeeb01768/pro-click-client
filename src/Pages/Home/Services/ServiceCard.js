23977fffc4b0d18b85e95b54ae2425a7240e8ac4
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-08 22:42:44 +0600

    card added

---
+++ src/Pages/Home/Services/ServiceCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ServiceCard = ({ service }) => {
+    const {image, price, service_name, details } = service;
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={image} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{service_name}</h2>
+                <p>{details}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('services.json')
-        .then (res => res.json())
-        .then(data => setServices(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
             <div className='text-center'>
-                <h2 className='text-3xl font-bold text-sky-700'>Our Packages</h2>
+                <h2 className='text-3xl font-bold text-sky-700'>My Photography Packages</h2>
             </div>
-            <div>
-                <h2>package : {services.length}</h2>;
+            <div className='grid grid-cols-1 mx-auto lg:grid-cols-3 gap-6 m-5'>
+                
                 {
-                    
+                    services.map(service => <ServiceCard
+                    key={service.services_id}
+                    service={service}
+                    ></ServiceCard>)
                 }
+            </div>
+            <div className='text-center'>
+            <button className='btn btn-primary my-5'><Link to=''></Link>See More</button>
             </div>
         </div>
     );
