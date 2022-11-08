883518f5d5b6d76dcb073710b20924867ea4b940
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-08 07:06:33 +0600

    json data and slider images added

--- src/Pages/Home/Slider/Slider.js
+++ src/Pages/Home/Slider/Slider.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import img1 from '../../../assets/images/slider/1.jpg'
-import img2 from '../../../assets/images/slider/2.jpg'
+import img1 from '../../../assets/images/slider/1.jpg';
+import img2 from '../../../assets/images/slider/2.jpg';
+import img3 from '../../../assets/images/slider/3.jpg';
+import img4 from '../../../assets/images/slider/4.jpg';
+import img5 from '../../../assets/images/slider/5.jpg';
+import img6 from '../../../assets/images/slider/6.jpg';
 
 const Slider = () => {
     return (
         <div>
-            {/* <div className="carousel w-full">
-                <div id="item1" className="carousel-item w-full ">
-                    <img src={img1} className="w-full " />
+            <div className="carousel w-full">
+                <div id="item1" className="carousel-item w-5/6 ">
+                    <img src={img1} alt='' className="w-full" />
                 </div>
                 <div id="item2" className="carousel-item w-full">
-                    <img src={img2} className="w-full" />
+                    <img src={img2} alt='' className="w-full" />
                 </div>
                 <div id="item3" className="carousel-item w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <img src={img3} alt='' className="w-full" />
                 </div>
                 <div id="item4" className="carousel-item w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <img src={img4} alt='' className="w-full" />
                 </div>
                 <div id="item5" className="carousel-item w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <img src={img5} alt='' className="w-full" />
                 </div>
                 <div id="item6" className="carousel-item w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <img src={img6} alt='' className="w-full" />
                 </div>
             </div>
             <div className="flex justify-center w-full py-2 gap-2">
@@ -32,7 +36,7 @@ const Slider = () => {
                 <a href="#item4" className="btn btn-xs">4</a>
                 <a href="#item5" className="btn btn-xs">5</a>
                 <a href="#item6" className="btn btn-xs">6</a>
-            </div> */}
+            </div>
         </div>
     );
 };
